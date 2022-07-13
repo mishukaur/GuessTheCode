42e6728fbf8da863d71dbfedb3429bc72f699716
Mishu Kaur <69322074+mishukaur@users.noreply.github.com>
2022-07-13 00:16:07 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+Code = []
+Guesses = ["_", "_", "_", "_"]
+Letters = ["A", "B", "C", "D", "E", "F"]
+for x in range(4):
+	Code.append(random.choice(Letters))
+
+print(Code)
+userinput = list(input("Guess the code!: "))
+
+while Code != userinput:
+	for x in range(len(userinput)):
+		if userinput[x] == Code[x]:
+			Guesses[x] = 1
+		elif userinput[x] in Code and userinput[x] != Code[x]:
+			Guesses[x] = 2
+		elif userinput[x] not in Code:
+			Guesses[x] = 3
+	print(Guesses)
+
+	if Code == userinput is True:
+		print("You guessed the code!")
+		break
+
+	userinput = list(input("Guess the code!: "))
